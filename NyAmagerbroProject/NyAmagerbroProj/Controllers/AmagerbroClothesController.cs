f4b0103913ddcda5b170814ecbce89b2301ec57e
Versacci <ahmed_farah_2@hotmail.com>
2017-08-29 14:13:22 +0200

    Added amagerbro store
    Amagerbro store added with repository

---
+++ NyAmagerbroProject/NyAmagerbroProj/Controllers/AmagerbroClothesController.cs
@@ -0,0 +1,91 @@
+﻿using NyAmagerbroProj.Models;
+using NyAmagerbroProj.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NyAmagerbroProj.Controllers
+{
+    public class AmagerbroClothesController : Controller
+    {
+        // GET: AmagerbroClothes
+        private IAmagerbroClothesRep<AmagerbroClothesStore> amagerRepository = null;
+
+        public AmagerbroClothesController()
+        {
+            this.amagerRepository = new AmagerbroClothesRep<AmagerbroClothesStore>();
+        }
+
+        // GET: Amagerbro
+        public ActionResult Index()
+        {
+            var AmagerbroClothesStore = amagerRepository.GetAll();
+            return View(AmagerbroClothesStore);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(AmagerbroClothesStore AmagerbroClothesStore)
+        {
+            if (ModelState.IsValid)
+            {
+                amagerRepository.Insert(AmagerbroClothesStore);
+                amagerRepository.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(AmagerbroClothesStore);
+            }
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var amagerbroStore = amagerRepository.GetById(Id);
+            return View(amagerbroStore);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(AmagerbroClothesStore AmagerbroClothesStore)
+        {
+            if (ModelState.IsValid)
+            {
+                amagerRepository.Update(AmagerbroClothesStore);
+                amagerRepository.Save();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(AmagerbroClothesStore);
+            }
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var AmagerbroClothesStore = amagerRepository.GetById(Id);
+            return View(AmagerbroClothesStore);
+        }
+
+        public ActionResult Delete(int Id)
+        {
+            var amagerbroStore = amagerRepository.GetById(Id);
+            return View(amagerbroStore);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int Id)
+        {
+            var AmagerbroClothesStore = amagerRepository.GetById(Id);
+            amagerRepository.Delete(Id);
+            amagerRepository.Save();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NyAmagerbroProject/NyAmagerbroProj/Models/AmagerbroDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace NyAmagerbroProj.Models
+{
+    public class AmagerbroDbContext : ApplicationDbContext
+    {
+        public AmagerbroDbContext()
+        {
+        }
+
+        public DbSet<AmagerbroClothesStore> AmagerbroClothesStore { get; set; }
+        public DbSet<AmagerbroElectronicsStore> AmagerbroElectronicsStore { get; set; }
+        public DbSet<AmagerbroFoodStore> AmagerbroFoodStore { get; set; }
+    }
+}
